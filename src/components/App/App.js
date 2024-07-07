4fdb06eb47accf540cd3509927b720dd692752c0
Mxolisi <mxolisi.mncube09@gmail.com>
2024-07-07 19:42:55 +0200

    Built App, Business, BusinessList and SearchBar components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-//import styles from "./App.module.css";
+import styles from "./App.module.css";
+import BusinessList from "../BusinessList/Businesslist";
+import SearchBar from "../SearchBar/SearchBar";
+
 
 //import BusinessList from "../BusinessList/BusinessList";
 //import SearchBar from "../SearchBar/SearchBar";
 
 const App = () => {
   return (
-    <div className={App.css}>
+    <div className={styles.App}>
       <h1>ravenous</h1>
+      <SearchBar/>
+      <BusinessList />
     </div>
   );
 };
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './Business.css';
+import styles from './Business.module.css';
 
 const Business = () =>{
     const business = {
-      imageSrc: 'https://www.lapergola.co.za/',
+      imageSrc: 'https://i.pinimg.com/736x/e7/99/7a/e7997a52c18102161ac0c012bfcdb14a.jpg',
       name: 'La Pergola',
-      address: '44 Stanley Ave, Braamfontein Werf',
+      address: '44 Stanley Ave, Braamfontein',
       city: 'Johannesburg',
-      Province:'Gauteng',
+      province:'Gauteng',
       zipcode: '2092',
       category: 'Italian',
       rating: 4.0,
@@ -15,27 +15,26 @@ const Business = () =>{
     };
   
     return (
-        <div className='business'>
-            <div className='image-container'>
+        <div className={styles.Business}>
+            <div className={styles.Imagecontainer}>
                 <img src={business.imageSrc} alt=''/>
             </div>
             <h2>{business.name}</h2>
-            <div className='business-information'>
-                <div className='business-address'>
+            <div className={styles.BusinessInfo}>
+                <div className={styles.BusinessAdress}>
                     <p>{business.address}</p>
                     <p>{business.city}</p>
-                    <p>{business.state} {business.zipcode}</p>
+                    <p>{`${business.province} ${business.zipcode}`}</p>
                 </div>
-                <div className='business-reviews'>
+                <div className={styles.BusinessReviews}>
                     <h3>{business.category}</h3>
-                    <h3 className='rating'>{`${business.rating} stars`}</h3>
+                    <h3 className={styles.rating}>{`${business.rating} stars`}</h3>
                     <p>{`${business.reviewCount} reviews`}</p>
                 </div>
             </div>
         </div>
-
     );
-  }
+  };
 
 
 export default Business;
